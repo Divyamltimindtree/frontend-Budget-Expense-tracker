dd6ed78e851560a0cf62a35ca1652ff132c359bf
Divyam Kumar Jha <divyam.10827581@ltimindtree.com>
2025-05-07 14:00:03 +0530

    login form updated and home page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+
+  imports: [RouterModule,CommonModule,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+   loginForm: FormGroup;
+   constructor(private fb: FormBuilder) { 
+     this.loginForm=this.fb.group({
+         email:['',[Validators.required,Validators.email]],
+         password:['',[Validators.required,Validators.minLength(6)]]
+     })
 
+  
+    
+   }
+   onSubmit(){
+    if(this.loginForm.valid){
+      console.log('Login successful',this.loginForm.value);
+      alert("successfull");
+    }else{
+      this.loginForm.markAllAsTouched();
+    }
+   }
 }
