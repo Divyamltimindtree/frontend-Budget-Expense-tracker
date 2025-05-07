b9dd2e01b3c96b50b7995313c215f066f28a7a8f
divdivi <divyashreeghegde@gmail.com>
2025-05-07 12:49:50 +0530

    Login Form Added

---
+++ frontend/src/app/component/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule],
+})
+export class LoginComponent {
+  loginForm !: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Login successful:', this.loginForm.value);
+      this.router.navigate(['/dashboard']); // Redirect after successful login
+    } else {
+      console.log('Invalid credentials');
+    }
+  }
+}
+
