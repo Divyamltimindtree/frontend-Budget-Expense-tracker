1d0a42d2e07f8a4ea88df5c10c758a10ee208cb1
ayushsingh5 <singhayush.mth@gmail.com>
2025-05-08 14:12:35 +0530

    dashboard

--- frontend/src/app/component/dashboard/dashboard.component.ts
+++ frontend/src/app/component/dashboard/dashboard.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NavbarComponent,CommonModule,RouterModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  constructor(private router: Router) {}
+  // constructor(private router: Router) {}
 
-  navigateTo(page: string) {
-    this.router.navigate([`/${page}`]);
-  }
+  // navigateTo(page: string) {
+  //   this.router.navigate([`/${page}`]);
+  // }
 
-  logout() {
-    // Implement your logout logic here
-    console.log('Logged out');
-  }
+  // logout() {
+  //   // Implement your logout logic here
+  //   console.log('Logged out');
+  // }
 }
