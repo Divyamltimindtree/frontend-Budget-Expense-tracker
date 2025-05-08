5403a7073aa008e239be82245a40d0e99a8d35a6
krayush1109 <hellotoayush@gmail.com>
2025-05-08 17:10:23 +0530

    Integrated: Add Income with api

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    
+    provideHttpClient(), // ✅ Add this line
+
   ]
 };
